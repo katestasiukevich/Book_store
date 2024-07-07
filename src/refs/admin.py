c4f0cb6bac76ea806abdf12288b18007f227ee0e
Kate Stasyukevich <kajsa1995@mail.ru>
2024-07-07 23:19:32 +0300

    author, genre, series

---
+++ src/refs/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+admin.site.register(models.Genre)
+admin.site.register(models.Series)
+admin.site.register(models.Author)
---
+++ src/refs/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+# Create your models here.
+class Genre(models.Model):
+    """class for genre references"""
+    name = models.CharField(max_length=100)
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+
+    def __str__(self) -> str:
+        return f"{self.pk}. {self.name}"
+    
+class Series(models.Model):
+    """class for series references"""
+    name = models.CharField(max_length=100)
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+class Author(models.Model):
+    """class for author references"""
+    name = models.CharField(max_length=100)
+    series = models.ForeignKey(Series, on_delete=models.PROTECT, related_name="authors")
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+    
