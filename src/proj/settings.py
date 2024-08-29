1ae1c63fa7f2a956837a4d72e02bc2ec86f59878
Kate Stasyukevich <kajsa1995@mail.ru>
2024-08-29 20:52:21 +0300

    login redirect fixed, home page cart added

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -68,7 +68,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'proj.urls'
 
-LOGIN_REDIRECT_URL = reverse_lazy('home')
+LOGIN_REDIRECT_URL = reverse_lazy('accounts:profile-create')
 LOGIN_URL = reverse_lazy('login')
 LOGOUT_URL = reverse_lazy('logout')
 
